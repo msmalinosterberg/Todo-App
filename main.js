94fe01d5632ecfa1a4ed239107f68a0621a292a6
Camilla <camilla.johansson1986@gmail.com>
2020-11-20 15:59:54 +0100

    js files added

---
+++ main.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', loadSite)
+
+const todoState () {[
+      title:
+      date:
+      ]
+
+}
+
+function main () {
+      loadToday();
+      loadTodos();
+      loadCalendar();
+      
+}
