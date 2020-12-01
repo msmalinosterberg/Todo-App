1a54c8b07b5eb7d41455f1476a14e16480d73a99
Camilla <camilla.johansson1986@gmail.com>
2020-12-01 17:27:43 +0100

    den fungerande koden

--- todos.js
+++ todos.js
@@ -1,111 +1,58 @@
 function loadTodos() {
-    // hämta alla todos från LS 
-        renderTodos(); 
-    }
-    
-     let todos = [{title: "test", date: "2020-11-06"}];
+    renderTodos();
+}
 
-    function renderTodos() {
-        const todoContainer = document.getElementById('todo-container')
-        todoContainer.innerHTML = ""; 
-    
-        for(const todo of todoState) {
-            const listItem = document.createElement('li'); 
-            listItem.innerHTML = todo.title; 
-            
-    
-            listItem.addEventListener('click', function(){
-                // tar bort en todo när man klickar på todon 
-                const index = todoState.indexOf(todo); 
-                todoState.splice(index, 1); 
-                renderTodos(); 
-                renderCalendar(); 
-                console.log(todoState); 
-            })
-            todoContainer.append(listItem); 
-        }
+let todos = [];
+
+function addTodoListener() {
+    document.getElementById('todoBtn').addEventListener('click', addTodo);
+}
+
+
+const addTodo = (ev) => {
+    ev.preventDefault();
+    let todo = {
+        title: document.getElementById('title').value,
+        date: document.getElementById('date').value
     }
     todos.push(todo);
     document.querySelector('form').reset();
     renderTodos();
+    updateCalenderWithTodoInfo();
     // console.log(todos);
-
+}
 
 
 function renderTodos() {
-    const todoContainer = document.getElementById('list-content')
+    const todoContainer = document.getElementById('list-content');
     todoContainer.innerHTML = "";
+    const updateCalenderWithTodoInfo = document.getElementById('time-div');
 
     for (const todoItem of todos) {
+        // create todo item div
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add("todo-item-div"); // add class
+        // todoDiv.appendChild(todoContainer);
+
+        // create li
         const listItem = document.createElement('li');
+        todoDiv.appendChild(listItem);
         listItem.innerHTML = todoItem.title;
-        const checkbox = document.createElement('input')
-        checkbox.type = 'checkbox'
-        //   const editPen = document.createElement('i')
-        //   const deleteTodoX = document.createElement('i')
+        // create trash button
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add("trash-button"); // add class
+        todoDiv.appendChild(trashButton);
 
-    function addTodoListener() {
-        document.getElementById('todoBtn').addEventListener('click', addTodo);
-      }
-      
-      let todos = [];
-      
-      const addTodo = (ev)=>{
-          ev.preventDefault();
-          let todo = {
-              title: document.getElementById('title').value,
-              date: document.getElementById('date').value
-          }
-          todos.push(todo);
-          document.querySelector('form').reset();
-          renderTodos();
-          console.log(todo);
-      }
-      
-      function loadTodos() {
-          // hämta alla todos från LS 
-              renderTodos(); 
-          }
-          
-          
-      function renderTodos() {
-          const todoContainer = document.getElementById('list-content')
-          todoContainer.innerHTML = ""; 
-      
-          for(const todoItem of todos) {
-              const listItem = document.createElement('li'); 
-              listItem.innerHTML = todoItem.title;
-              const checkbox = document.createElement('input')
-              checkbox.type = 'checkbox'
-            //    const editPen = document.createElement('i')
-            //    const deleteTodoX = document.createElement('i')
-
-      
-              listItem.addEventListener('click', function(){
-                  // tar bort en todo när man klickar på todon 
-                  const index = todos.indexOf(todoItem); 
-                  todos.splice(index, 1); 
-                  renderTodos(); 
-              })
-            //  todoContainer.append(checkbox, listItem, editPen, deleteTodoX); 
-                todoContainer.append(checkbox, listItem); 
-          }
-      }
-
-
-
-
-      // HUr många arrayer finns det med datumet xxx
-
-        listItem.addEventListener('click', function () {
-            // tar bort en todo när man klickar på todon 
+        // remove todo when click checkbox
+        trashButton.addEventListener('click', function () {
             const index = todos.indexOf(todoItem);
             todos.splice(index, 1);
             renderTodos();
-            updateCalenderWithTodoInfo();
+            // updateCalenderWithTodoInfo();
         })
-        //   todoContainer.append(checkbox, listItem, editPen, deleteTodoX); 
-        todoContainer.append(checkbox, listItem);
+        listItem.append(trashButton);
+        todoContainer.append(listItem);
     }
 }
 
